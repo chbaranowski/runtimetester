41200dc59f95d8e13d5465e7fb2c2bc4b3b94df0
Christian Baranowski <tux2323@pro-pix.de>
2010-02-21 14:54:33 -0700

    remove whitespace from Integrated Test Annotation

--- de.propix.runtimetester.core/src/de/propix/runtimetester/core/IntegratedTest.java
+++ de.propix.runtimetester.core/src/de/propix/runtimetester/core/IntegratedTest.java
@@ -10,6 +10,4 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
-public @interface IntegratedTest {
-	
-}
+public @interface IntegratedTest {}
